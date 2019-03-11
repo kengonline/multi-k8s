5fc12df57a76e618404e08f588824b84d4cddc8b
kengonline <keang2537@hotmail.com>
2019-03-11 17:54:57 +0700

    Add travis deployment script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t kengonline/k8s-frontend:latest -t kengonline/k8s-frontend:$SHA -f ./frontend/Dockerfile ./frontend
+docker build -t kengonline/k8s-backend-server:latest kengonline/k8s-backend-server:$SHA -f ./backend-server/Dockerfile ./backend-server
+docker build -t kengonline/k8s-worker:latest -t kengonline/k8s-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push kengonline/k8s-frontend:latest
+docker push kengonline/k8s-frontend:$SHA
+
+docker push kengonline/k8s-backend-server:latest
+docker push kengonline/k8s-backend-server:$SHA
+
+docker push kengonline/k8s-worker:latest
+docker push kengonline/k8s-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/frontend-deployment server=kengonline/k8s-frontend:$SHA
+kubectl set image deployments/backend-server-deployment server=kengonline/k8s-backend-server:$SHA
+kubectl set image deployments/worker-deployment server=kengonline/k8s-worker:$SHA
