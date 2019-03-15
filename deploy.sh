b868fa49aa19f0cb48a3355884f20d79f85ff904
kengonline <keang2537@hotmail.com>
2019-03-15 16:38:55 +0700

    Fixed syntax error build k8s-backend-server

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
-echo "Build image"
 docker build -t kengonline/k8s-frontend:latest -t kengonline/k8s-frontend:$SHA -f ./frontend/Dockerfile ./frontend
-docker build -t kengonline/k8s-backend-server:latest kengonline/k8s-backend-server:$SHA -f ./backend-server/Dockerfile ./backend-server
+docker build -t kengonline/k8s-backend-server:latest -t kengonline/k8s-backend-server:$SHA -f ./backend-server/Dockerfile ./backend-server
 docker build -t kengonline/k8s-worker:latest -t kengonline/k8s-worker:$SHA -f ./worker/Dockerfile ./worker
 
-echo "Push image to docker hub"
 docker push kengonline/k8s-frontend:latest
 docker push kengonline/k8s-frontend:$SHA
 
@@ -13,7 +11,6 @@ docker push kengonline/k8s-backend-server:$SHA
 docker push kengonline/k8s-worker:latest
 docker push kengonline/k8s-worker:$SHA
 
-echo " Apply config to k8s"
 kubectl apply -f k8s
 kubectl set image deployments/frontend-deployment server=kengonline/k8s-frontend:$SHA
 kubectl set image deployments/backend-server-deployment server=kengonline/k8s-backend-server:$SHA
