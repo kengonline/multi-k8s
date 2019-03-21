676c681ae855b35d04eca95d9ed300c954df8935
kengonline <keang2537@hotmail.com>
2019-03-21 15:57:07 +0700

    Change container name

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ docker push kengonline/k8s-worker:latest
 docker push kengonline/k8s-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/frontend-deployment web=kengonline/k8s-frontend:$SHA
-kubectl set image deployments/backend-server-deployment backend-server=kengonline/k8s-backend-server:$SHA
+kubectl set image deployments/frontend-deployment frontend=kengonline/k8s-frontend:$SHA
+kubectl set image deployments/backend-server-deployment backend=kengonline/k8s-backend-server:$SHA
 kubectl set image deployments/worker-deployment worker=kengonline/k8s-worker:$SHA
