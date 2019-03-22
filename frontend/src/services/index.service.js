5a1c6777d86f9a68957c572c4476bef13bd26cac
kengonline <keang2537@hotmail.com>
2019-03-22 14:43:00 +0700

    Change api endpoint

--- frontend/src/services/index.service.js
+++ frontend/src/services/index.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const getValues = () => axios.get('/api/values/current');
+export const getValues = () => axios.get('api.bookstore.business/values/current');
 
-export const getIndexes = () => axios.get('/api/values/all');
+export const getIndexes = () => axios.get('api.bookstore.business/values/all');
 
-export const addIndex = (index) => axios.post('/api/values', { index });
+export const addIndex = (index) => axios.post('api.bookstore.business/values', { index });
