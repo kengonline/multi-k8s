7fbcfff563276e450e0e0669dc4eb4de5cfb333d
kengonline <keang2537@hotmail.com>
2019-03-22 14:48:39 +0700

    Fixed incorrect api url

--- frontend/src/services/index.service.js
+++ frontend/src/services/index.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const getValues = () => axios.get('api.bookstore.business/values/current');
+export const getValues = () => axios.get('https://api.bookstore.business/values/current');
 
-export const getIndexes = () => axios.get('api.bookstore.business/values/all');
+export const getIndexes = () => axios.get('https://api.bookstore.business/values/all');
 
-export const addIndex = (index) => axios.post('api.bookstore.business/values', { index });
+export const addIndex = (index) => axios.post('https://api.bookstore.business/values', { index });
